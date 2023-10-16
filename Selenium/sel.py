af901d116ff7eca45e5577bdd9999f57f029b9fe
techtenant <bnyilima@gmail.com>
2023-10-16 17:59:57 +0300

    Adding awaiter time

--- Selenium/sel.py
+++ Selenium/sel.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+import time
 
 chrome_driver_path = 'c:\Program Files\Google\Chrome\Application\chrome.exe'
 url = "https://www.youtube.com/"
 driver = webdriver.Chrome()
 driver.get(url)
+time.sleep(40)
 driver.quit()
 
 
